eb52219acdf5a3af73304da2804bd8886a57e73d
Djakh <basitxanov@gmail.com>
2022-03-12 14:42:28 +0500

    Issue #1: loading page was implemented

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,12 @@
+import 'package:ecogram/bloc/app.dart';
 import 'package:ecogram/routes.dart';
+import 'package:ecogram/switcher.dart';
 import 'package:ecogram/theme/style.dart';
 import 'package:ecogram/theme/theme.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EcogramApp extends StatefulWidget {
   const EcogramApp({Key key}) : super(key: key);
@@ -14,6 +17,7 @@ class EcogramApp extends StatefulWidget {
 
 class _EcogramAppState extends State<EcogramApp> {
   final router = FluroRouter();
+  bool isLoader = true;
 
   /// --- Life Cycles ---
 
@@ -22,28 +26,58 @@ class _EcogramAppState extends State<EcogramApp> {
     super.initState();
     AppRoutes.defineRoutes(router);
     Application.router = router;
+    changeLoader();
+  }
+
+  /// --- Methods ---
+
+  void changeLoader() async {
+    await Future.delayed(Duration(seconds: 1), () => isLoader = false);
+    setState(() {});
   }
 
   /// --- Widgets ---
 
-  @override
-  Widget build(BuildContext context) => CupertinoApp(
-      theme: theme,
-      color: Style.colors.white,
-      localizationsDelegates: const [
-        DefaultMaterialLocalizations.delegate,
-        DefaultCupertinoLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-      ],
-      onGenerateRoute: Application.router.generator,
-      home: TestPage());
-}
+  Widget imageBox(String image, double height, double width) => Container(
+        height: height,
+        width: width,
+        child: Image.asset(image, fit: BoxFit.contain),
+      );
 
-class TestPage extends StatelessWidget {
-  const TestPage({Key key}) : super(key: key);
+  Widget get loader => SafeArea(
+        child: Container(
+          color: Style.colors.white,
+          padding: Style.padding16,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 162),
+              imageBox("assets/images/leaf.png", 96, 96),
+              imageBox("assets/images/ecogram_text.png", 87, 256),
+            ],
+          ),
+        ),
+      );
 
   @override
-  Widget build(BuildContext context) {
-    return Container(color: Style.colors.white,);
-  }
+  Widget build(BuildContext context) => MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => AppCubit()),
+          ],
+          child: CupertinoApp(
+              theme: theme,
+              color: Style.colors.white,
+              localizationsDelegates: const [
+                DefaultMaterialLocalizations.delegate,
+                DefaultCupertinoLocalizations.delegate,
+                DefaultWidgetsLocalizations.delegate,
+              ],
+              onGenerateRoute: Application.router.generator,
+              home: Container(
+                color: Style.colors.white,
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 500),
+                  child: isLoader ? loader : Switcher(),
+                ),
+              )));
 }
---
+++ lib/asset_manager.dart
@@ -0,0 +1,9 @@
+class AssetManager {
+  static final String onBoardScreen1 = "assets/images/onboard_screen1.png";
+  static final String onBoardScreen2 = "assets/images/onboard_screen2.png";
+  static final String onBoardScreen3 = "assets/images/onboard_screen3.png";
+  static final String profile = "assets/images/profile.png";
+  static final String phone = "assets/images/phone.png";
+  static final String conversation = "assets/images/conversation.png";
+
+}
